ce7672d95e4b4591596bd67fab8755a4b0d2d5f5
Odd Marthon Lende <oddmarthon.lende@bakerhughes.com>
2013-07-02 01:23:36 +0200

    Improved schemas and routing

--- plots/sample.5.js
+++ plots/sample.5.js
@@ -16,7 +16,7 @@ Object.extend(Plot.datasets, {
 			"type": 'cubism',
 			"height" : "50%",
 			"width" : "100%",
-			"bufferSize" : 60 * 60 * 24, // 24 hours of 1 second data
+			"bufferSize" : 60 * 60 * 24 * 2, // 24 hours of 500ms data
 			"title": "Example Cubism Visualization with schema",
 			"description": "",
 			"step" : 1e4,
@@ -25,17 +25,61 @@ Object.extend(Plot.datasets, {
 				"Math.[function]" : {
 					
 					"bind" : {
-						"Math.function" : "label",
-						"function" : "value"
+						"Math.[function]" : "label",
+						"[function]" : {
+							"[function]" : "components[[function]]",
+							"sqrt" : "functions[0]",
+							"cos" : "functions[1]",
+							"sin" : "functions[2]",
+							"random" : "functions[3]"
+						},
+						"{value}" : "value",
+						"{letter}" : "letter"
 					},
 					"defaults" : {
-						"timestamp" : function() { return +new Date(); }
+						"components" : Object,
+						"functions" : Array,
+						"function" : String,
+						"letter" : function(d) { return d?String(d).toLowerCase():String();},
+						"timestamp" : function(d) { return d?d:+new Date(); }
 					}
 				
 				}
 
 			}
 
+		},
+		
+	1 : {
+
+			"type": 'graph',
+			"width" : "50%",
+			"height" : "50%",
+            "labels" : ["sqrt", "cos", "sin", "random"],
+            "title": "Example Graph with schema",
+			"description": "Example Graph",
+			"step" : 1000,
+			"schema" : {
+				
+				"Math.[function]" : {
+					
+					"bind" : {
+						"[function]" : {
+							"{value}" : {
+								"sqrt" : "values[0]",
+								"cos" : "values[1]",
+								"sin" : "values[2]",
+								"random" : "values[3]"
+							}
+						}
+					},
+					"defaults" : {
+						"name" : "Math"
+					}
+				
+				}
+
+			}
 		}
 		
 });
