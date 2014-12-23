4bb103a67a237f045bdff6fbc8678a7fa86c86aa
Odd Marthon Lende <oddmarthon.lende@bakerhughes.com>
2014-12-23 13:08:51 +0100

    brightness: add uniform to control max brightness

--- lib/types/browser/lib/shaders/effect.brightness.frag.c
+++ lib/types/browser/lib/shaders/effect.brightness.frag.c
@@ -13,6 +13,7 @@ precision highp float;
 uniform sampler2D sampler;
 uniform vec2 resolution;
 
+uniform float brightness;
 uniform float alpha;
 uniform float cutoff;
 
@@ -21,14 +22,14 @@ vec3 weight = vec3(1./3., 1./3., 1./3.);
 void main () {
     
     vec4 result = texture2D(sampler, gl_FragCoord.xy / resolution);
-    float a = (1. - alpha);
+    float a = (1. - alpha) * brightness;
     
     if( dot(result.rgb,  weight) > cutoff )
        result = vec4(
-            a * (1. - result.r) + result.r,
-            a * (1. - result.g) + result.g,
-            a * (1. - result.b) + result.b,
-            result.a
+            (a * (1. - result.r)) + result.r,
+            (a * (1. - result.g)) + result.g,
+            (a * (1. - result.b)) + result.b,
+            1.
         );
        
     gl_FragColor = result;
